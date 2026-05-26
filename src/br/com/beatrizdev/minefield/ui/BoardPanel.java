d93c043f52f6ea14b92d146fbcf53c4662693454
Beatriz Farias Péres <devbeatrizfarias.bfp@gmail.com>
2026-05-26 17:31:28 -0300

    feat: implement Minesweeper graphical interface #01

---
+++ src/br/com/beatrizdev/minefield/ui/BoardPanel.java
@@ -0,0 +1,13 @@
+package br.com.beatrizdev.minefield.ui;
+
+import javax.swing.JPanel;
+
+import br.com.beatrizdev.minefield.model.Board;
+
+@SuppressWarnings("serial")
+public class BoardPanel extends JPanel {
+
+	public BoardPanel(Board board) {
+		
+	}
+}
---
+++ src/br/com/beatrizdev/minefield/ui/MainScreen.java
@@ -0,0 +1,25 @@
+package br.com.beatrizdev.minefield.ui;
+
+import javax.swing.JFrame;
+
+import br.com.beatrizdev.minefield.model.Board;
+
+@SuppressWarnings("serial")
+public class MainScreen extends JFrame {
+	
+	public MainScreen() {
+		Board board = new Board(16, 30, 50);
+		
+		add(new BoardPanel(board));
+		
+		setTitle("Minefield");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new MainScreen();
+	}
+}
